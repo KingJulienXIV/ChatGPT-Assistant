a957274bfb31a1faf550795c6aa85881b404fe1a
Marjet Garcia <marjet@garcia.net.au>
2023-01-13 12:28:26 +1000

    Modified main function
    Main function was modified to more optimally extract the student's info

--- main.py
+++ main.py
@@ -29,34 +29,38 @@ def main():
         Dr. Vitor Botazzi is available on Tuesdays and Thursdays.
         The following is a conversation with an AI assistant. 
         The assistant is helpful, creative, clever, and very friendly.\n
-        The assistant will try to get the student's name, email, and mobile\n\n
-        AI: Hello I'm a chatbot assistant, how may I assist you today?\n
-        Student:
+        The assistant will initiate the conversation by asking the student for
+        their name, email, and mobile\n
+        It will ask what day the student will be arriving in the office and
+        reply with whether Dr. Botazzi is available on that day.\n\n
+        Ai:
         """
   
     # conversation with chatGPT
     text_message = ""
 
     while(text_message.lower()!="goodbye"): # conversation ends with a goodbye
-        # get text input from student
-        text_message = input()
-        prompt += f"{text_message} \nAi:"
 
         # get response from chatgpt
         response = generate_response(prompt) + "\nStudent:"
         print(response)
         prompt += response
+
+        # get text input from student
+        text_message = input()
+        prompt += f"{text_message} \nAi:"
     
     
     # telling chatGPT to extract info from conversation
+    conversation = prompt.split("day.\n\n")[1]
+
     INFO = ["name", "email", "mobile"]
-    get_student_info_prompt = f"Extract the Student {INFO[0]} from this conversation:\n\n"
 
-
-    get_student_info_prompt += prompt
-    guest_name = generate_response(get_student_info_prompt)
-    guest_email = generate_response(get_student_info_prompt)
-    guest_mobile = generate_response(get_student_info_prompt)
+    get_student_info_prompt = "Tell me the Student's {} from this conversation:\n\n" + conversation
+ 
+    guest_name = generate_response(get_student_info_prompt.format(INFO[0]))
+    guest_email = generate_response(get_student_info_prompt.format(INFO[1]))
+    guest_mobile = generate_response(get_student_info_prompt.format(INFO[2]))
 
     print(guest_name, guest_email, guest_mobile)
 
